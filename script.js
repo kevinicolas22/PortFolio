9a4c5353034e8d5a7bf5c5780e0074b0dd2fbe48
Kevin <106784104+Kevin2220@users.noreply.github.com>
2022-08-26 15:37:55 -0300

    portfolio em andamento

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     });
+
+    $('.menu-btn').click(function(){
+     $('.navbar .menu').toggleClass("active");
+     $('.menu-btn i').toggleClass("active");
+    });
 });
