24449da536648b4c48cae16630baaa49a9d0c196
Kevin <106784104+Kevin2220@users.noreply.github.com>
2022-10-26 09:57:46 -0300

    final

--- script.js
+++ script.js
@@ -5,10 +5,51 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["Desenvolvedor", "Front-End", "Back-End", "Freelancer"],
+         typeSpeed:50,
+         backSpeed:50,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Desenvolvedor", "Front-End", "Back-End"],
+        typeSpeed:50,
+        backSpeed:50,
+        loop:true
     });
 
     $('.menu-btn').click(function(){
-     $('.navbar .menu').toggleClass("active");
-     $('.menu-btn i').toggleClass("active");
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
     });
 });
