a9bfc2edcb09c9dd4c04c7b82c3b6fa52806fe83
Kevin <106784104+Kevin2220@users.noreply.github.com>
2024-01-03 18:08:16 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,55 +1,60 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-     $('.scroll-up-btn').click(function(){
-         $('html').animate({scrollTop: 0});
-     });
-
-     var typed = new Typed(".typing", {
-         strings:["Desenvolvedor", "Front-End", "Back-End",],
-         typeSpeed:180,
-         backSpeed:90,
-         loop:true
-     });
-     var typed = new Typed(".typing-2", {
-        strings:["JS", "React", "Java", "Python", "C", "Git"],
-        typeSpeed:180,
-        backSpeed:90,
-        loop:true
+$(document).ready(function () {
+    // Navbar Sticky
+    $(window).scroll(function () {
+        // Adiciona ou remove a classe "sticky" dependendo da posição do scroll
+        $('.navbar').toggleClass("sticky", this.scrollY > 20);
+        // Mostra ou esconde o botão de scroll dependendo da posição do scroll
+        $('.scroll-up-btn').toggleClass("show", this.scrollY > 500);
     });
 
-    $('.menu-btn').click(function(){
+    // Botão de Scroll Up
+    $('.scroll-up-btn').click(function () {
+        // Anima o scroll para o topo da página
+        $('html').animate({ scrollTop: 0 });
+    });
+
+    // Typed.js para o título principal
+    var mainTyped = new Typed(".typing", {
+        strings: ["Desenvolvedor", "Full Stack"],
+        typeSpeed: 180,
+        backSpeed: 90,
+        loop: true
+    });
+
+    // Typed.js para o título secundário
+    var secondaryTyped = new Typed(".typing-2", {
+        strings: ["JavaScript", "Java", "Python", "C"],
+        typeSpeed: 180,
+        backSpeed: 90,
+        loop: true
+    });
+
+    // Alternar Menu da Navbar
+    $('.menu-btn').click(function () {
+        // Adiciona ou remove a classe "active" do menu e do ícone do botão
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // Owl Carousel para Projetos
     $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
             },
-            600:{
-                items:2,
-                nav:false
+            600: {
+                items: 2,
+                nav: false
             },
-            1000:{
-                items:3,
-                nav:false
+            1000: {
+                items: 3,
+                nav: false
             }
         }
-    }); 
+    });
 });
